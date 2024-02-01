fc33177796670e04c126d082696dd43de19d6098
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-02-01 22:42:56 +0500

    Add files via upload

---
+++ 012 function/index.js
@@ -0,0 +1,29 @@
+// let a = 3;
+// let b = 49;
+
+function bilal(x, y) {
+  console.log(v + w);
+}
+bilal(3, 4); // calling a function
+
+// // ES6 ---- ECMA SCRIT
+let add = () => {
+  console.log(a + b);
+};
+
+add();
+
+function sadeeq() {
+  let a = 2;
+  let b = 12;
+  let add = a + b;
+  let mul = a * b;
+  let div = a / b;
+
+  return { add, mul, div };
+}
+console.log(sadeeq());
+
+let rasheed = () => {
+  console.log(" my name is rasheed ahmad");
+};
